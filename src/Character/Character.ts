3df19de4ef294fb12b9a6855044ce34168326907
alfeukramer <alfeukramer@gmail.com>
2022-10-19 15:17:32 -0300

    Ajuste Lint

--- src/Character/Character.ts
+++ src/Character/Character.ts
@@ -1,13 +1,13 @@
-import Archetype from '../Archetypes';
-import Fighter from '../Fighter';
+// import Archetype from '../Archetypes';
+// import Fighter from '../Fighter';
 
-export default class Character implements Fighter {
-    private _race: Race;
-    private _archetype: Archetype;
-    private _maxLifePoints: number;
-    private _lifePoints: number;
-    private _strength: number;
-    private _defense: number;
-    private _dexterity: number;
-    private _energy: Energy;
-}
+// export default class Character implements Fighter {
+//   private _race: Race;
+//   private _archetype: Archetype;
+//   private _maxLifePoints: number;
+//   private _lifePoints: number;
+//   private _strength: number;
+//   private _defense: number;
+//   private _dexterity: number;
+//   private _energy: Energy;
+// }
